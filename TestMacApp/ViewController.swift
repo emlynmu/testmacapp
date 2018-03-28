51a5645fb378beb7573038467bbd59239682c9e9
Emlyn Murphy <me@emlyn.net>
2018-03-28 12:14:49 -0400

    Start with a macOS app template from Xcode

---
+++ TestMacApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestMacApp
+//
+//  Created by Emlyn Murphy on 3/28/18.
+//  Copyright © 2018 Black Pixel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
